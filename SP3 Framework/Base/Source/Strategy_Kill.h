2ae351580b1fbffb3fc8670e2acb1ea4cd59f199
wei heng lim <imweihengnyp@gmail.com>
2015-08-22 23:36:09 +0800

    Day 6 - Made the current state of enemy accessible

--- SP3 Framework/Base/Source/Strategy_Kill.h
+++ SP3 Framework/Base/Source/Strategy_Kill.h
@@ -18,25 +18,11 @@ public:
 	void SetDestination(const float x, const float y);
 	void SetEnemyPosition(const float x, const float y);
 	void GetEnemyPosition(float& x, float& y);
-
-	//The AI states of the enemy
-	enum CURRENT_STATE
-	{
-		PATROL,
-		REPEL,
-		ATTACK,
-		NUM_ENEMY_STATE,
-	};
-
-	void SetState(CStrategy_Kill::CURRENT_STATE);
-	CStrategy_Kill::CURRENT_STATE GetState();
 	
 	string pathFind (const int& xStart, const int& yStart, const int& xFinish, const int& yFinish, vector<CGoodies*> goodyList);
 
 	bool detectionCheck(CMap* map, Vector3& enemyTile, Vector3& heroTile, Vector3& enemyDir);
 private:
-	//Enemy AI State
-	CStrategy_Kill::CURRENT_STATE CurrentState;
 
 	string route, route2;
 	int routeCounter, routeCounter2;
